c173bab3377409e5f7fd3ed42afb5f7eb300eb57
Domenic Denicola <d@domenic.me>
2021-09-18 15:44:33 -0400

    Update lint config
    Notably includes moving from four-space indents to two.

--- scripts/generate-grammar.js
+++ scripts/generate-grammar.js
@@ -3,10 +3,10 @@ const waka = require("waka");
 
 let grammarText = "";
 process.stdin.on("data", data => {
-    grammarText += data;
+  grammarText += data;
 });
 process.stdin.on("end", () => {
-    const parserSource = waka.getSourceStandalone(grammarText, { startRule: null });
-    process.stdout.write("module.exports = ");
-    process.stdout.write(parserSource);
+  const parserSource = waka.getSourceStandalone(grammarText, { startRule: null });
+  process.stdout.write("module.exports = ");
+  process.stdout.write(parserSource);
 });
