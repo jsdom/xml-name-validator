76513f2be3cc997351e47b6581bd2d9cd2fe49e0
Domenic Denicola <d@domenic.me>
2021-09-18 15:44:33 -0400

    Update lint config
    Notably includes moving from four-space indents to two.

--- test/test.js
+++ test/test.js
@@ -3,37 +3,37 @@ const xnv = require("..");
 const assert = require("assert");
 
 describe("Valid names", () => {
-    for (const validName of ["x", ":", "a:0", "a0", "a:b:c"]) {
-        specify(`"${validName}" is recognized as a valid name`, () => {
-            assert.strictEqual(xnv.name(validName).success, true);
-        });
-    }
+  for (const validName of ["x", ":", "a:0", "a0", "a:b:c"]) {
+    specify(`"${validName}" is recognized as a valid name`, () => {
+      assert.strictEqual(xnv.name(validName).success, true);
+    });
+  }
 });
 
 describe("Invalid names/qnames", () => {
-    for (const invalidName of ["\\", "'", "\"", "0", "0:a", "a!", ""]) {
-        specify(`"${invalidName}" is recognized as an invalid name`, () => {
-            assert.strictEqual(xnv.name(invalidName).success, false);
-        });
+  for (const invalidName of ["\\", "'", "\"", "0", "0:a", "a!", ""]) {
+    specify(`"${invalidName}" is recognized as an invalid name`, () => {
+      assert.strictEqual(xnv.name(invalidName).success, false);
+    });
 
-        specify(`"${invalidName}" is recognized as an invalid qname`, () => {
-            assert.strictEqual(xnv.qname(invalidName).success, false);
-        });
-    }
+    specify(`"${invalidName}" is recognized as an invalid qname`, () => {
+      assert.strictEqual(xnv.qname(invalidName).success, false);
+    });
+  }
 });
 
 describe("Valid qualified names", () => {
-    for (const validQname of ["x", "a0", "a:b"]) {
-        specify(`"${validQname}" is recognized as a valid qname`, () => {
-            assert.strictEqual(xnv.qname(validQname).success, true);
-        });
-    }
+  for (const validQname of ["x", "a0", "a:b"]) {
+    specify(`"${validQname}" is recognized as a valid qname`, () => {
+      assert.strictEqual(xnv.qname(validQname).success, true);
+    });
+  }
 });
 
 describe("Invalid qualified names", () => {
-    for (const invalidQname of [":a", "b:", "x:y:z", "a:0", "emp:", ":", "_:", ":0a", "_::a", "prefix::local"]) {
-        specify(`"${invalidQname}" is recognized as an invalid name`, () => {
-            assert.strictEqual(xnv.qname(invalidQname).success, false);
-        });
-    }
+  for (const invalidQname of [":a", "b:", "x:y:z", "a:0", "emp:", ":", "_:", ":0a", "_::a", "prefix::local"]) {
+    specify(`"${invalidQname}" is recognized as an invalid name`, () => {
+      assert.strictEqual(xnv.qname(invalidQname).success, false);
+    });
+  }
 });
